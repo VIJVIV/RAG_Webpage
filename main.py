dd82bb2df1d56bf6bd22871b7eb43d4fb7a99f3d
VJVIV <94681456+VJVIV@users.noreply.github.com>
2024-02-27 11:47:53 +0100

    Code clean, chunk exp, qa modify

--- main.py
+++ main.py
@@ -7,52 +7,69 @@ from langchain.vectorstores import Chroma
 from langchain.llms import HuggingFaceHub
 from langchain.chains import RetrievalQA
 
-HF_token = getpass()
-os.environ['HUGGINGFACEHUB_API_TOKEN'] = HF_token
-
-URL = 'https://blogs.nvidia.com/blog/what-is-retrieval-augmented-generation/'
-
-#Read and load data from specified webpage
-data = WebBaseLoader(URL)
-content = data.load()
-print(content)
-
 #Text splitter - creating 'chunks of data'
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=50)  
-chunking = text_splitter.split_documents(content)
-print(len(chunking))
+def text_splitter(content):  
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=50) 
+    chunks = text_splitter.split_documents(content)
+    print('Number of chunks created - ', len(chunks))   #Chunk size is number of characters, not tokens
+    for i, _ in enumerate(chunks):
+        print(f'chunk # {i}, {chunks[i]}')
+        print('----------------------------------------')
+    return chunks
 
-#Embedding model - Refer to MTEB HuggingFace leaderboard for choosing appropriate embedding model 
-embeddings = HuggingFaceInferenceAPIEmbeddings(api_key = HF_token, model_name = 'BAAI/bge-base-en-v1.5')
+#Embedding generation
+def embedding(HF_token, embedding_model): 
+    embeddings = HuggingFaceInferenceAPIEmbeddings(api_key = HF_token, model_name = embedding_model)
+    return embeddings
 
 #Vector db creation
-vector_db = Chroma.from_documents(chunking, embeddings)
-print('Done')
+def vector_database(chunks, embeddings):
+    vector_db = Chroma.from_documents(chunks, embeddings)
+    print('vector db created with embeddings')
+    return vector_db
 
-#Model 
-model = HuggingFaceHub(repo_id='HuggingFaceH4/zephyr-7b-alpha', model_kwargs={'temperature': 0.5, 
-                                                                              'max_new_tokens': 512,
-                                                                              'max_length': 64})
 
-#Step 1 - Retrieval from vector db
-retriever = vector_db.as_retriever(search_type='mmr', search_kwargs={'k':1})   #Retrieves k relevant chunks
-#Tune k value according to content, for complex data such as pdf/collection of pdf's, k can be 5-10
+if __name__ == '__main__':
+    HF_token = getpass()
+    os.environ['HUGGINGFACEHUB_API_TOKEN'] = HF_token
 
-#Step 2 - Augment - Prompt template
-query = "Did NVIDIA develop an AI workflow for retrieval augmented generation? What does it include?"
+    URL = 'https://blogs.nvidia.com/blog/what-is-retrieval-augmented-generation/'
 
-prompt = f'''
-<|system|>
-You are an AI assistant that follows instructions extremely well. Please be truthful and give direct answers
-</s>
-<|user|>
-{query}
-</s>
-<|chatbot|>
-'''
+    embedding_model_name = 'BAAI/bge-base-en-v1.5' #Refer MTEB HuggingFace leaderboard for choosing embedding model
 
-#Step 3 - Generation - Connect LLM with retrieved document chunk
-qa = RetrievalQA.from_chain_type(llm=model, retriever=retriever, chain_type='stuff')
-response = qa(prompt)
-print(response['result'])
+    #Read and load data from specified webpage
+    data = WebBaseLoader(URL)
+    content = data.load()
+
+    #Chunking, embedding and vector database creation
+    chunking = text_splitter(content)
+    embeddings = embedding(HF_token, embedding_model_name)
+    vector_db = vector_database(chunking, embeddings)
+
+    #LLM Model 
+    model = HuggingFaceHub(repo_id='HuggingFaceH4/zephyr-7b-alpha', model_kwargs={'temperature': 0.5, 
+                                                                                  'max_new_tokens': 512,
+                                                                                  'max_length': 64})
+    
+    #Step 1 - Retrieval from vector db
+    retriever = vector_db.as_retriever(search_type='mmr', search_kwargs={'k':1})   #Retrieves k relevant chunks
+    #Tune k value according to content, for complex data such as pdf/collection_of_pdf's, k can be 5-10
+
+    #Step 2 - Augment - Prompt template
+    query = "Did NVIDIA develop an AI workflow for retrieval augmented generation? What does it include?"
+
+    prompt = f'''
+    <|system|>
+    You are an AI assistant that follows instructions extremely well. Please be truthful and give direct answers
+    </s>
+    <|user|>
+    {query}
+    </s>
+    <|chatbot|>
+    '''
+
+    #Step 3 - Generation - Connect LLM with retrieved document chunk
+    qa = RetrievalQA.from_chain_type(llm=model, retriever=retriever, chain_type='refine')
+    response = qa(prompt)
+    print(response['result'])
 
